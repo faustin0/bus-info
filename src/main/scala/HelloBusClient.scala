283685e102d56e40cdabf6896b398e8d1ceb1c7e
Fausto Di Natale <fausto.dinatale@ynap.com>
2020-07-09 18:17:15 +0200

    using IO as return type,  scalafmt formatting

--- src/main/scala/HelloBusClient.scala
+++ src/main/scala/HelloBusClient.scala
@@ -1,7 +1,7 @@
 import java.time.format.DateTimeFormatter
 
 import cats.effect.IO
-import models.{BusRequest, HelloBusResponse, TransformError}
+import models.{BusRequest, HelloBusResponse}
 import org.http4s.Method._
 import org.http4s.client._
 import org.http4s.client.dsl.io._
@@ -12,43 +12,44 @@ import org.http4s.{Headers, MediaType, Request, UrlForm}
 
 import scala.xml.Elem
 
-
 //val URL_HELLO_BUS = "QueryHellobus"
 //val CONTENT_TYPE = "application/x-www-form-urlencoded"
 
 case class HelloBusClient(private val httpClient: Client[IO]) {
   private val dateTimePattern = DateTimeFormatter.ofPattern("HHmm")
 
+  private val targetUri =
+    uri"https://hellobuswsweb.tper.it/web-services/hello-bus.asmx/QueryHellobus"
 
-  def hello(busRequest: BusRequest): IO[Either[TransformError, HelloBusResponse]] = {
-
-    val target = uri"https://hellobuswsweb.tper.it/web-services/hello-bus.asmx/QueryHellobus"
+  def hello(busRequest: BusRequest): IO[HelloBusResponse] = {
 
     val request = Request[IO](
       method = POST,
-      uri = target,
+      uri = targetUri,
       headers = Headers.of(Accept(MediaType.application.xml))
-    ).withEntity(UrlForm(
-      "fermata" -> busRequest.busStop.toString,
-      "linea" -> busRequest.busID,
-      "oraHHMM" -> busRequest.hour.format(dateTimePattern)
-    ))
+    ).withEntity(
+      UrlForm(
+        "fermata" -> busRequest.busStop.toString,
+        "linea" -> busRequest.busID,
+        "oraHHMM" -> busRequest.hour.format(dateTimePattern)
+      )
+    )
 
-    httpClient
-      .expect[Elem](request)
-      .map(HelloBusResponse.fromXml)
+    for {
+      xmlResponse <- httpClient.expect[Elem](request)
+      parsed <- IO.fromEither(HelloBusResponse.fromXml(xmlResponse))
+    } yield parsed
   }
 
   //TODO remove test method
   def sample(): IO[String] = {
     val target = uri"https://jsonplaceholder.typicode.com/todos/1"
-    val request = GET(
-      target
-    )
+    val request = GET(target)
     httpClient.expect[String](request)
   }
 }
 
 object HelloBusClient {
-  def apply(httpClient: Client[IO]): HelloBusClient = new HelloBusClient(httpClient)
+  def apply(httpClient: Client[IO]): HelloBusClient =
+    new HelloBusClient(httpClient)
 }
