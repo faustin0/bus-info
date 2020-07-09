cedaaebc478647397076b20f333c7593ac62c2a2
Fausto Di Natale <fausto.dinatale@ynap.com>
2020-07-09 18:19:36 +0200

    refactoring tests

--- src/test/scala/HelloBusClientTest.scala
+++ src/test/scala/HelloBusClientTest.scala
@@ -1,60 +1,54 @@
 import java.time.LocalTime
 import java.util.concurrent.Executors
 
-import cats.effect.{Blocker, ContextShift, IO, Timer}
-import models.{BusRequest, Invalid, NoBus}
+import cats.effect.testing.scalatest.AsyncIOSpec
+import cats.effect.{Blocker, IO}
+import models.{BusRequest, HelloBusResponse, Invalid, NoBus}
 import org.http4s.client.{Client, JavaNetClientBuilder}
-import org.scalatest.{BeforeAndAfter, FunSuite, Inside, Matchers}
+import org.scalatest.freespec.AsyncFreeSpec
+import org.scalatest.matchers.should.Matchers
 
 import scala.concurrent.ExecutionContext
-import scala.concurrent.ExecutionContext.global
 
+class HelloBusClientTest extends AsyncFreeSpec with AsyncIOSpec with Matchers {
 
-class HelloBusClientTest extends FunSuite with BeforeAndAfter with Matchers with Inside {
-
-  implicit val cs: ContextShift[IO] = IO.contextShift(global)
-  implicit val timer: Timer[IO] = IO.timer(global)
-
-  var sut: HelloBusClient = _
-
-  before {
-    val blockingEC = ExecutionContext.fromExecutorService(Executors.newFixedThreadPool(5))
+  def createFixture = {
+    val blockingEC =
+      ExecutionContext.fromExecutorService(Executors.newFixedThreadPool(5))
     val blocker = Blocker.liftExecutorService(blockingEC)
     val httpClient: Client[IO] = JavaNetClientBuilder[IO](blocker).create
-    sut = HelloBusClient(httpClient)
+    HelloBusClient(httpClient)
   }
 
-  test("should execute sample request ") {
-    val actual = sut.sample().unsafeRunSync()
-    actual.shouldNot(have size 0)
-  }
+  "should execute sample tper request " in {
 
+    val sut = createFixture
+    val actual = sut.hello(BusRequest("27", 303))
 
-  test("should execute sample tper request ") {
-    val actual = sut.hello(BusRequest("27", 303)).unsafeRunSync()
-
-    println(actual)
-    inside(actual) { case Right(response) =>
-      response should not be a[Invalid]
+    actual.asserting {
+      case _: HelloBusResponse => succeed
+      case _                   => fail()
     }
   }
 
-  test("should execute sample tper request barrato") {
-    val actual = sut.hello(BusRequest("85/", 303, LocalTime.of(20, 20))).unsafeRunSync()
+  "should execute sample tper request barrato" in {
+    val sut = createFixture
+    val actual =
+      sut.hello(BusRequest("85/", 303, LocalTime.of(20, 20)))
 
-    println(actual)
-    inside(actual) { case Right(response) =>
-      response should not be a[Invalid]
-      response shouldBe a[NoBus]
+    actual.asserting {
+      case r: NoBus => succeed
+      case _        => fail()
     }
   }
 
-  test("should get Invalid when malformed bus") {
-    val actual = sut.hello(BusRequest("x", 303)).unsafeRunSync()
+  "should get Invalid when malformed bus" in {
+    val sut = createFixture
+    val actual = sut.hello(BusRequest("x", 303))
 
-    println(actual)
-    inside(actual) { case Right(response) =>
-      response shouldBe a[Invalid]
+    actual.asserting {
+      case Invalid(_)   => succeed
+      case t: Throwable => fail(t)
     }
   }
 }
--- src/test/scala/models/HelloBusResponseTest.scala
+++ src/test/scala/models/HelloBusResponseTest.scala
@@ -1,10 +1,12 @@
 package models
 
-import org.scalatest.{FunSuite, Inside, Matchers}
+import org.scalatest.Inside
+import org.scalatest.funsuite.AnyFunSuite
+import org.scalatest.matchers.should.Matchers
 
 import scala.xml.Elem
 
-class HelloBusResponseTest extends FunSuite with Inside with Matchers {
+class HelloBusResponseTest extends AnyFunSuite with Inside with Matchers {
 
   val successfulResponse: Elem =
     <string xmlns="https://hellobuswsweb.tper.it/web-services/hellobus.asmx">
@@ -26,7 +28,6 @@ class HelloBusResponseTest extends FunSuite with Inside with Matchers {
       TperHellobus: OGGI NESSUNA ALTRA CORSA DI 1 PER FERMATA 303
     </string>
 
-
   test("should parse xml from tper into successful response ") {
 
     val r1 = BusResponse("28", true, "09:07", "(Bus5517 CON PEDANA)")
@@ -34,45 +35,50 @@ class HelloBusResponseTest extends FunSuite with Inside with Matchers {
     val expected = Successful(List(r1, r2))
     val result = HelloBusResponse.fromXml(successfulResponse)
 
-
-    inside(result) { case Right(response) =>
-      response.shouldBe(expected)
+    inside(result) {
+      case Right(response) =>
+        response.shouldBe(expected)
     }
   }
 
-  test("should parse xml from tper into successful response when hour is present") {
+  test(
+    "should parse xml from tper into successful response when hour is present"
+  ) {
 
     val r1 = BusResponse("27A", false, "21:59", "(Bus6567 CON PEDANA)")
     val r2 = BusResponse("27B", false, "22:29", "(Bus6584 CON PEDANA)")
     val expected = Successful(List(r1, r2))
     val result = HelloBusResponse.fromXml(successfulResponseWithHour)
 
-    inside(result) { case Right(response) =>
-      response.shouldBe(expected)
+    inside(result) {
+      case Right(response) =>
+        response.shouldBe(expected)
     }
   }
 
   test("should parse helloBusHelp into Invalid response") {
 
-    val expected = Invalid("HellobusHelp: LINEA ASD NON GESTITA, indicare FERMATA LINEA o FERMATA LINEA ORA. Esempi: 4004 27A , 4004 27A 0810")
+    val expected = Invalid(
+      "HellobusHelp: LINEA ASD NON GESTITA, indicare FERMATA LINEA o FERMATA LINEA ORA. Esempi: 4004 27A , 4004 27A 0810"
+    )
     val result = HelloBusResponse.fromXml(HellobusHelp)
 
-
-    inside(result) { case Right(response) =>
-      response.shouldBe(expected)
+    inside(result) {
+      case Right(response) =>
+        response.shouldBe(expected)
     }
   }
 
   test("should parse response when no more buses into NoBus response") {
 
-    val expected = NoBus("TperHellobus: OGGI NESSUNA ALTRA CORSA DI 1 PER FERMATA 303")
+    val expected =
+      NoBus("TperHellobus: OGGI NESSUNA ALTRA CORSA DI 1 PER FERMATA 303")
     val result = HelloBusResponse.fromXml(noBus)
 
-
-    inside(result) { case Right(response) =>
-      response.shouldBe(expected)
+    inside(result) {
+      case Right(response) =>
+        response.shouldBe(expected)
     }
   }
-
 
 }
