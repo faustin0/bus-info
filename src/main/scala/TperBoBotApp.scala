0a27d26a95ed832460381a84da95a86faa1444c7
faustin0 <dinatalefausto@gmail.com>
2020-07-21 09:34:50 +0200

    handling query parameters for bus and hour filtering adding scalafmt

--- src/main/scala/TperBoBotApp.scala
+++ src/main/scala/TperBoBotApp.scala
@@ -1,14 +1,18 @@
-import cats.effect.{ExitCode, IO, IOApp, Resource}
+import java.time.LocalTime
+
+import cats.data.Validated
+import cats.effect.{ExitCode, IO, IOApp}
+import io.circe.generic.auto._
 import models.{BusRequest, Invalid, NoBus, Successful}
-import org.http4s.HttpRoutes
+import org.http4s.circe.CirceEntityEncoder._
 import org.http4s.client.blaze.BlazeClientBuilder
+import org.http4s.client.middleware.{Logger => ClientLogger}
 import org.http4s.dsl.impl.Root
 import org.http4s.dsl.io._
 import org.http4s.implicits._
 import org.http4s.server.blaze.BlazeServerBuilder
 import org.http4s.server.middleware.Logger
-import org.http4s.circe.CirceEntityEncoder._
-import io.circe.generic.auto._
+import org.http4s.{HttpRoutes, ParseResult, QueryParamDecoder}
 
 import scala.concurrent.ExecutionContext.global
 import scala.concurrent.duration._
@@ -16,15 +20,12 @@ import scala.concurrent.duration._
 class Routes(helloBusClient: HelloBusClient) {
   val helloBusService = HttpRoutes
     .of[IO] {
-      case GET -> Root / "hello" / IntVar(busStop) =>
-        for {
-          resp <- helloBusClient.hello(BusRequest("27", busStop))
-          restResp <- resp match {
-            case r: NoBus      => NotFound(r)
-            case r: Invalid    => BadRequest(r)
-            case r: Successful => Ok(r.buses)
-          }
-        } yield restResp
+      case GET -> Root / "hello" / IntVar(busStop) :? BusNumber(bus) +& Hour(hour) =>
+        hour match {
+          case Some(Validated.Valid(h))   => execRequest(BusRequest(busStop, bus, Some(h)))
+          case Some(Validated.Invalid(e)) => BadRequest(e.map(_.sanitized))
+          case None                       => execRequest(BusRequest(busStop, bus))
+        }
 
       case GET -> Root / "hello" / "" =>
         BadRequest(Invalid("missing busStop path"))
@@ -33,15 +34,36 @@ class Routes(helloBusClient: HelloBusClient) {
         BadRequest(Invalid(s"Invalid busStop: $invalid"))
     }
     .orNotFound
+
+  private val execRequest = (busRequest: BusRequest) => {
+    for {
+      resp <- helloBusClient.hello(busRequest)
+      restResp <- resp match {
+        case r: NoBus      => NotFound(r)
+        case r: Invalid    => BadRequest(r)
+        case r: Successful => Ok(r.buses)
+      }
+    } yield restResp
+  }
+
+  object BusNumber extends OptionalQueryParamDecoderMatcher[String]("bus")
+  object Hour extends OptionalValidatingQueryParamDecoderMatcher[LocalTime]("hour")
+
+  implicit val timeQueryDecoder: QueryParamDecoder[LocalTime] =
+    QueryParamDecoder[String].emap(
+      h => ParseResult.fromTryCatchNonFatal(s"Invalid date format '$h'")(LocalTime.parse(h))
+    )
+
 }
 
 object TperBoBotApp extends IOApp {
 
   override def run(args: List[String]): IO[ExitCode] = {
-    val routes: Resource[IO, Routes] = BlazeClientBuilder[IO](global)
+    val routes = BlazeClientBuilder[IO](global)
       .withConnectTimeout(5 seconds)
-      .withRequestTimeout(5 seconds)
+      .withRequestTimeout(7 seconds)
       .resource
+      .map(client => ClientLogger(logHeaders = false, logBody = true)(client))
       .map(client => HelloBusClient(client))
       .map(tperClient => new Routes(tperClient))
 
