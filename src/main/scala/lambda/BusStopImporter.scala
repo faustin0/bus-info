9952c64a11bf80f5e93ca30c95cd0216f3c28e30
faustin0 <dinatalefausto@gmail.com>
2020-08-12 22:11:23 +0200

    lambda outcome logging

--- src/main/scala/lambda/BusStopImporter.scala
+++ src/main/scala/lambda/BusStopImporter.scala
@@ -1,11 +1,14 @@
 package lambda
 
+import java.io.{PrintWriter, StringWriter}
+
 import cats.effect.{IO, Resource}
-import com.amazonaws.services.dynamodbv2.{AmazonDynamoDB, AmazonDynamoDBClientBuilder}
+import com.amazonaws.services.dynamodbv2.datamodeling.DynamoDBMapper.FailedBatch
+import com.amazonaws.services.dynamodbv2.{AmazonDynamoDB, AmazonDynamoDBAsyncClientBuilder, AmazonDynamoDBClientBuilder}
 import com.amazonaws.services.lambda.runtime.events.S3Event
 import com.amazonaws.services.lambda.runtime.events.models.s3.S3EventNotification.S3EventNotificationRecord
 import com.amazonaws.services.lambda.runtime.{Context, RequestHandler}
-import com.amazonaws.services.s3.model.{GetObjectRequest, S3Object, S3Event => S3EventType}
+import com.amazonaws.services.s3.model.{GetObjectRequest, S3Object}
 import com.amazonaws.services.s3.{AmazonS3, AmazonS3ClientBuilder}
 import fs2.Stream
 import models.{BusStop, TransformError}
@@ -27,26 +30,24 @@ class BusStopImporter(
   override def handleRequest(s3Event: S3Event, context: Context): Unit = {
     val logger = context.getLogger
     val repo   = new BusStopRepository(dynamoClient)
+    s3Event.getRecords.forEach(it => logger.log(s"S3 event: ${it.getEventName}"))
 
-    s3Event.getRecords.asScala
-      .find(e => e.getEventName.equals(S3EventType.ObjectCreated.toString))
+    val outcome = s3Event.getRecords.asScala
+      .find(e => e.getEventName.contains("ObjectCreated:")) // todo there must be a better way ...
       .map { e =>
         readBucketObjectFromEvent(e)
           .use(obj => {
             for {
               elem     <- parseS3Object(obj)
               busStops <- BusStopImporter.streamParseXmlDataset(elem).compile.toList
-              failed   <- repo.batchInsert(busStops).compile.toList
-            } yield (failed)
+              failed <- repo.batchInsert(busStops).find(_.getUnprocessedItems.size() > 0).compile.toList
+            } yield ImporterOutcome(obj.getKey, busStops.length, failed.headOption)
           })
       }
-      .getOrElse(IO.pure(Nil))
-      .attempt
-      .unsafeRunSync() match {
-      case Left(err)            => logger.log(s"Error processing $s3Event: $err ${err.getStackTrace}")
-      case Right(Nil)           => logger.log(s"Successfully processed $s3Event")
-      case Right(failedInserts) => logger.log(s"Failed to save in dynamoDB: $failedInserts")
-    }
+      .getOrElse(IO.pure(ImporterOutcome("No file to process", 0)))
+      .unsafeRunSync()
+
+    logger.log(outcome.toString)
   }
 
   private def readBucketObjectFromEvent(e: S3EventNotificationRecord): Resource[IO, S3Object] = {
@@ -81,4 +82,27 @@ object BusStopImporter {
       .fromIterator[IO]((xml \\ "NewDataSet" \\ "Table").iterator)
       .evalMapChunk(t => IO.fromEither(BusStop.fromXml(t)))
   }
+}
+case class ImporterOutcome(
+  processedFile: String,
+  processedItems: Int,
+  failure: Option[FailedBatch] = None
+) {
+
+  private def getStackTrace(t: Throwable): String = {
+    val sw = new StringWriter()
+    val pw = new PrintWriter(sw)
+    t.printStackTrace(pw)
+    sw.toString
+  }
+
+  private def formatException(t: Throwable): String = s"$t ${getStackTrace(t)}"
+
+  override def toString: String =
+    s"""
+       |processed file: '$processedFile'
+       |extracted items count: '$processedItems'
+       |failed inserts count: '${failure.map(_.getUnprocessedItems.size()).getOrElse(0)}'
+       |${failure.map(f => formatException(f.getException)).getOrElse("")}
+       |""".stripMargin
 }
