b4cbbc3d8263d40eb8085efb6dc8a165dc7c43ff
faustin0 <dinatalefausto@gmail.com>
2020-09-03 23:43:17 +0200

    adding bus stops info route, renaming root path

--- src/main/scala/Routes.scala
+++ src/main/scala/Routes.scala
@@ -7,28 +7,27 @@ import models.{BusRequest, Invalid, NoBus, Successful}
 import org.http4s.circe.CirceEntityEncoder._
 import org.http4s.dsl.impl.Root
 import org.http4s.dsl.io._
-import org.http4s.implicits._
 import org.http4s.{HttpRoutes, ParseResult, QueryParamDecoder}
+import repositories.BusStopRepository
 
-class Routes(helloBusClient: HelloBusClient) {
+class Routes(private val helloBusClient: HelloBusClient) {
   val helloBusService = HttpRoutes
     .of[IO] {
-      case GET -> Root / "hello" / IntVar(busStop) :? BusNumber(bus) +& Hour(hour) =>
+      case GET -> Root / "bus-stops" / IntVar(busStop) :? BusNumber(bus) +& Hour(hour) =>
         hour match {
           case Some(Validated.Valid(h))   => execRequest(BusRequest(busStop, bus, Some(h)))
           case Some(Validated.Invalid(e)) => BadRequest(e.map(_.sanitized))
           case None                       => execRequest(BusRequest(busStop, bus))
         }
 
-      case GET -> Root / "hello" / "" =>
+      case GET -> Root / "bus-stops" / "" =>
         BadRequest(Invalid("missing busStop path"))
 
-      case GET -> Root / "hello" / invalid =>
+      case GET -> Root / "bus-stops" / invalid =>
         BadRequest(Invalid(s"Invalid busStop: $invalid"))
     }
-    .orNotFound
 
-  private val execRequest = (busRequest: BusRequest) => {
+  private def execRequest(busRequest: BusRequest) = {
     for {
       resp <- helloBusClient.hello(busRequest)
       restResp <- resp match {
@@ -42,8 +41,22 @@ class Routes(helloBusClient: HelloBusClient) {
   object BusNumber extends OptionalQueryParamDecoderMatcher[String]("bus")
   object Hour      extends OptionalValidatingQueryParamDecoderMatcher[LocalTime]("hour")
 
-  implicit val timeQueryDecoder: QueryParamDecoder[LocalTime] =
+  private implicit val timeQueryDecoder: QueryParamDecoder[LocalTime] =
     QueryParamDecoder[String].emap(h =>
       ParseResult.fromTryCatchNonFatal(s"Invalid date format '$h'")(LocalTime.parse(h))
     )
+}
+
+class InfoRoutes(private val busStopRepository: BusStopRepository) {
+  val busInfoService = HttpRoutes
+    .of[IO] {
+      case GET -> Root / "bus-stops" / IntVar(busStopCode) / "info" =>
+        busStopRepository
+          .findBusStopByCode(busStopCode.toLong)
+          .value
+          .flatMap {
+            case Some(busStop) => Ok(busStop)
+            case None          => NotFound(s"no bus stop with code $busStopCode")
+          }
+    }
 }
