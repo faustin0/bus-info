1c4f410d61c51e6ce3c16ad9cc44db76af1831d8
faustin0 <dinatalefausto@gmail.com>
2020-09-07 18:36:50 +0200

    adding bus stop routes to app, adding "/api" entrypoint

--- src/main/scala/BusInfoApp.scala
+++ src/main/scala/BusInfoApp.scala
@@ -1,8 +1,11 @@
-import cats.effect.{ExitCode, IO, IOApp}
+import cats.effect.{ExitCode, IO, IOApp, Resource}
 import org.http4s.client.blaze.BlazeClientBuilder
 import org.http4s.client.middleware.{Logger => ClientLogger}
+import org.http4s.implicits._
+import org.http4s.server.Router
 import org.http4s.server.blaze.BlazeServerBuilder
 import org.http4s.server.middleware.Logger
+import repositories.BusStopRepository
 
 import scala.concurrent.ExecutionContext.global
 import scala.concurrent.duration._
@@ -10,17 +13,31 @@ import scala.concurrent.duration._
 object BusInfoApp extends IOApp {
 
   override def run(args: List[String]): IO[ExitCode] = {
-    val routes = BlazeClientBuilder[IO](global)
+    val helloBusClientRes = BlazeClientBuilder[IO](global)
       .withConnectTimeout(5 seconds)
       .withRequestTimeout(7 seconds)
       .resource
       .map(client => ClientLogger(logHeaders = false, logBody = true)(client))
       .map(client => HelloBusClient(client))
-      .map(tperClient => new Routes(tperClient))
 
-    routes
-      .use(routes => {
-        val app       = routes.helloBusService
+    val application = for {
+      tperClient <- helloBusClientRes
+      busStopRepo <- Resource.liftF(
+        IO.fromTry(
+          BusStopRepository
+            .makeFromAws()
+            .orElse(BusStopRepository.makeFromEnv())
+        )
+      )
+      helloBusRoutes = new Routes(tperClient)
+      busInfoRoutes  = new InfoRoutes(busStopRepo)
+    } yield Router(
+      "/api" -> helloBusRoutes.helloBusService,
+      "/api" -> busInfoRoutes.busInfoService
+    ).orNotFound
+
+    application
+      .use(app => {
         val loggedApp = Logger.httpApp(logHeaders = false, logBody = true)(app)
 
         BlazeServerBuilder[IO](global)
