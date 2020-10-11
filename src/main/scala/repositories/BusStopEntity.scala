fc8f42d92bb92caa09ed531abe1a48f0d41fd316
faustin0 <dinatalefausto@gmail.com>
2020-10-11 15:43:32 +0200

    fu..ing intellij auto reformat 2

--- src/main/scala/repositories/BusStopEntity.scala
+++ src/main/scala/repositories/BusStopEntity.scala
@@ -8,18 +8,18 @@ import scala.beans.BeanProperty
 
 @DynamoDBTable(tableName = "bus_stops")
 case class BusStopEntity(
-                          @(DynamoDBHashKey@field)
-                          @(DynamoDBAttribute@field) @BeanProperty var code: Int,
-                          @(DynamoDBIndexHashKey@field)(globalSecondaryIndexName = "name-index")
-                          @(DynamoDBAttribute@field) @BeanProperty var name: String,
-                          @(DynamoDBAttribute@field) @BeanProperty var location: String,
-                          @(DynamoDBAttribute@field) @BeanProperty var comune: String,
-                          @(DynamoDBAttribute@field) @BeanProperty var areaCode: Int,
-                          @(DynamoDBAttribute@field) @BeanProperty var lat: Double,
-                          @(DynamoDBAttribute@field) @BeanProperty var lng: Double,
-                          @(DynamoDBAttribute@field) @BeanProperty var x: Long,
-                          @(DynamoDBAttribute@field) @BeanProperty var y: Long
-                        ) {
+  @(DynamoDBHashKey @field)
+  @(DynamoDBAttribute @field) @BeanProperty var code: Int,
+  @(DynamoDBIndexHashKey @field)(globalSecondaryIndexName = "name-index")
+  @(DynamoDBAttribute @field) @BeanProperty var name: String,
+  @(DynamoDBAttribute @field) @BeanProperty var location: String,
+  @(DynamoDBAttribute @field) @BeanProperty var comune: String,
+  @(DynamoDBAttribute @field) @BeanProperty var areaCode: Int,
+  @(DynamoDBAttribute @field) @BeanProperty var lat: Double,
+  @(DynamoDBAttribute @field) @BeanProperty var lng: Double,
+  @(DynamoDBAttribute @field) @BeanProperty var x: Long,
+  @(DynamoDBAttribute @field) @BeanProperty var y: Long
+) {
   def this() = this(0, "", "", "", -1, 0, 0, 0, 0)
 
   def as[A](implicit m: BusStopEntity => A): A = m(this)
