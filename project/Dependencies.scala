ac62527326e5cac83aa5145803420f006da4d0c5
Fausto Di Natale <dinatalefausto@gmail.com>
2021-02-21 06:10:37 +0000

    Update tapir-core, tapir-http4s-server, ... to 0.17.12

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -12,7 +12,7 @@ object Dependencies {
   val awsLambdaVersion           = "1.2.1"
   val s3sdkVersion               = "1.11.954"
   val awsLambdaJavaEventsVersion = "3.7.0"
-  val tapirVersion               = "0.17.10"
+  val tapirVersion               = "0.17.12"
   val dynamodbVersion            = "1.11.954"
   val logbackVersion             = "1.2.3"
 
