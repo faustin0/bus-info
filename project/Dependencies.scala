b820a667b46345c558b473f089cc7ffb114bc4f1
Fausto Di Natale <dinatalefausto@gmail.com>
2021-02-21 06:10:33 +0000

    Update aws-java-sdk-dynamodb to 1.11.959

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -10,10 +10,10 @@ object Dependencies {
   val scalaXmlVersion            = "1.3.0"
   val log4catsVersion            = "1.1.1"
   val awsLambdaVersion           = "1.2.1"
-  val s3sdkVersion               = "1.11.954"
+  val s3sdkVersion               = "1.11.959"
   val awsLambdaJavaEventsVersion = "3.7.0"
   val tapirVersion               = "0.17.10"
-  val dynamodbVersion            = "1.11.954"
+  val dynamodbVersion            = "1.11.959"
   val logbackVersion             = "1.2.3"
 
   lazy val testDependencies = Seq(
