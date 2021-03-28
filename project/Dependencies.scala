e141c6e65adb77f50b13df0563923922c6a8fc81
Fausto Di Natale <dinatalefausto@gmail.com>
2021-03-28 06:07:21 +0000

    Update aws-java-sdk to 1.11.986

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -18,7 +18,7 @@ object Dependencies {
   lazy val testDependencies = Seq(
     "org.scalatest"  %% "scalatest"                          % "3.2.6"                    % Test,
     "com.dimafeng"   %% "testcontainers-scala-localstack-v2" % testcontainersScalaVersion % Test,
-    "com.amazonaws"   % "aws-java-sdk"                       % "1.11.975"                 % Test, //needed by localstack
+    "com.amazonaws"   % "aws-java-sdk"                       % "1.11.986"                 % Test, //needed by localstack
     "com.dimafeng"   %% "testcontainers-scala-scalatest"     % testcontainersScalaVersion % Test,
     "com.codecommit" %% "cats-effect-testing-scalatest"      % "0.5.2"                    % Test,
     "org.typelevel"  %% "cats-effect-laws"                   % catsVersion                % Test
