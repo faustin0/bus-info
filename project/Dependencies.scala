b3f4f86af545966138dd3e822d9c338bd77b7ec4
Fausto Di Natale <dinatalefausto@gmail.com>
2021-05-23 06:17:35 +0000

    Update cats-effect, cats-effect-laws to 3.1.1

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -3,7 +3,7 @@ import sbt._
 object Dependencies {
   val http4sVersion              = "0.21.22"
   val testcontainersScalaVersion = "0.39.3"
-  val catsVersion                = "2.5.1"
+  val catsVersion                = "3.1.1"
   val kindProjectorV             = "0.11.3"
   val betterMonadicForV          = "0.3.1"
   val circeVersion               = "0.13.0"
