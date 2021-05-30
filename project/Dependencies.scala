87800cffae0b8c6eaa2d07a00bd7b1a5195fcb9e
Fausto Di Natale <dinatalefausto@gmail.com>
2021-05-30 07:10:40 +0000

    Update dynamodb, s3 to 2.16.74

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -9,7 +9,7 @@ object Dependencies {
   val circeVersion         = "0.13.0"
   val scalaXmlVersion      = "1.3.0"
   val log4catsVersion      = "1.1.1"
-  val awsSdkVersion        = "2.16.68"
+  val awsSdkVersion        = "2.16.74"
   val tapirVersion         = "0.18.0-M11"
   val logbackVersion       = "1.2.3"
   val fs2Version           = "2.5.6"
