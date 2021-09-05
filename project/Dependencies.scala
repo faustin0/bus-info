a0e8e70fc63ba9fa126caa26948da126fa497a63
Fausto Di Natale <dinatalefausto@gmail.com>
2021-09-05 06:12:49 +0000

    Update http4s-blaze-client, ... to 0.21.28

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -1,7 +1,7 @@
 import sbt._
 
 object Dependencies {
-  val http4sVersion        = "0.21.26"
+  val http4sVersion        = "0.21.28"
   val testcontainersScalaV = "0.39.5"
   val catsVersion          = "2.5.3"
   val kindProjectorV       = "0.13.1"
