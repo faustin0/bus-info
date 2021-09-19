50f9b0d721447fa4cf06003f4f3834adec6c5292
Fausto Di Natale <dinatalefausto@gmail.com>
2021-09-19 06:12:48 +0000

    Update scalatest to 3.2.10

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -16,7 +16,7 @@ object Dependencies {
 
 
   lazy val testDependencies = Seq(
-    "org.scalatest" %% "scalatest"                          % "3.2.9"              % Test,
+    "org.scalatest" %% "scalatest"                          % "3.2.10"              % Test,
     "com.dimafeng"  %% "testcontainers-scala-localstack-v2" % testcontainersScalaV % Test,
     "com.amazonaws"  % "aws-java-sdk"                       % "1.12.66"            % Test, //needed by localstack
     "com.dimafeng"  %% "testcontainers-scala-scalatest"     % testcontainersScalaV % Test,
