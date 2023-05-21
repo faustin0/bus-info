10ba3cca107cf410d970f34cf18540d58e4367c6
Fausto Di Natale <dinatalefausto@gmail.com>
2023-05-21 06:13:01 +0000

    Update aws-lambda-java-events to 3.11.2

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -67,7 +67,7 @@ object Dependencies {
 
   lazy val awsDeps = Seq(
     "com.amazonaws"          % "aws-lambda-java-core"   % "1.2.2",
-    "com.amazonaws"          % "aws-lambda-java-events" % "3.11.1",
+    "com.amazonaws"          % "aws-lambda-java-events" % "3.11.2",
     "software.amazon.awssdk" % "s3"                     % awsSdkVersion
   ) ++ dynamoDeps
 
