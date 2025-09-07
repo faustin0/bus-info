1cf8550f1cdbed3e4a45e8db4dec3fcadd3c7c8a
Fausto Di Natale <dinatalefausto@gmail.com>
2025-09-07 06:19:12 +0000

    Update aws-lambda-java-core to 1.4.0

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -76,13 +76,13 @@ object Dependencies {
   lazy val lambdaRuntimeDeps = "com.amazonaws" % "aws-lambda-java-runtime-interface-client" % "2.4.1"
 
   lazy val awsLambdaDeps = Seq(
-    "com.amazonaws" % "aws-lambda-java-core"   % "1.3.0",
+    "com.amazonaws" % "aws-lambda-java-core"   % "1.4.0",
     "com.amazonaws" % "aws-lambda-java-events" % "3.11.3"
   )
 
   @deprecated
   lazy val awsDeps = Seq(
-    "com.amazonaws"          % "aws-lambda-java-core"   % "1.3.0",
+    "com.amazonaws"          % "aws-lambda-java-core"   % "1.4.0",
     "com.amazonaws"          % "aws-lambda-java-events" % "3.16.0",
     "software.amazon.awssdk" % "s3"                     % awsSdkVersion
   ) ++ dynamoDeps
